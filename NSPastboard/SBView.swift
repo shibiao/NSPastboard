21412d6d9c6dfeebd3050880f491b73c119eaea2
WalkWithWind <956035125@qq.com>
2017-01-19 10:24:30 +0800

    1

---
+++ NSPastboard/SBView.swift
@@ -0,0 +1,38 @@
+//
+//  SBView.swift
+//  NSPastboard
+//
+//  Created by sycf_ios on 2017/1/19.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+import AVFoundation
+class SBView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        self.register(forDraggedTypes: [kUTTypeURL as String])
+        // Drawing code here.
+    }
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        let pasteboard = sender.draggingPasteboard()
+        let filteringOptions = [NSPasteboardURLReadingFileURLsOnlyKey:kUTTypeAudiovisualContent];
+        if pasteboard.canReadObject(forClasses: [NSURL.self], options: filteringOptions) {
+            
+            return NSDragOperation.copy
+        }
+        return NSDragOperation()
+        
+    }
+    override func draggingEnded(_ sender: NSDraggingInfo?) {
+        
+    }
+    override func draggingExited(_ sender: NSDraggingInfo?) {
+        
+    }
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        Swift.print("kk")
+        return true
+    }
+}
---
+++ NSPastboard/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NSPastboard
+//
+//  Created by sycf_ios on 2017/1/19.
+//  Copyright © 2017年 sycf_ios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
