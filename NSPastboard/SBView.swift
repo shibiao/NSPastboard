442225456ff6d2841429833fd96db52883d3cf77
WalkWithWind <956035125@qq.com>
2017-01-20 17:43:36 +0800

    first commit

--- NSPastboard/SBView.swift
+++ NSPastboard/SBView.swift
@@ -10,36 +10,95 @@ import Cocoa
 import AVFoundation
 class SBView: NSView {
 
+//    override func draw(_ dirtyRect: NSRect) {
+//        super.draw(dirtyRect)
+//        self.register(forDraggedTypes: [kUTTypeURL as String])
+//        // Drawing code here.
+//    }
+//    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+//        let pasteboard = sender.draggingPasteboard()
+//        let filteringOptions = [NSPasteboardURLReadingFileURLsOnlyKey:kUTTypeAudiovisualContent];
+//        if pasteboard.canReadObject(forClasses: [NSURL.self], options: filteringOptions) {
+//            
+//            return NSDragOperation.copy
+//        }
+//        return NSDragOperation()
+//        
+//    }
+//    override func draggingEnded(_ sender: NSDraggingInfo?) {
+//        
+//    }
+//    override func draggingExited(_ sender: NSDraggingInfo?) {
+//        
+//    }
+//    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+//        Swift.print("kk")
+//        return true
+//    }
+//    override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+//        
+//        return true
+//    }
+//    override func concludeDragOperation(_ sender: NSDraggingInfo?) {
+//        
+//    }
+    var filePath: String?
+    let expectedExt = ["kext"]  //file extensions allowed for Drag&Drop
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.wantsLayer = true
+        self.layer?.backgroundColor = NSColor.gray.cgColor
+        
+        register(forDraggedTypes: [kUTTypeURL as String])
+    }
+    
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
-        self.register(forDraggedTypes: [kUTTypeURL as String])
         // Drawing code here.
     }
+    
     override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
-        let pasteboard = sender.draggingPasteboard()
-        let filteringOptions = [NSPasteboardURLReadingFileURLsOnlyKey:kUTTypeAudiovisualContent];
-        if pasteboard.canReadObject(forClasses: [NSURL.self], options: filteringOptions) {
-            
-            return NSDragOperation.copy
+        if checkExtension(sender) == true {
+            self.layer?.backgroundColor = NSColor.blue.cgColor
+            return .copy
+        } else {
+            return NSDragOperation()
         }
-        return NSDragOperation()
-        
     }
-    override func draggingEnded(_ sender: NSDraggingInfo?) {
+    
+    fileprivate func checkExtension(_ drag: NSDraggingInfo) -> Bool {
+        guard let board = drag.draggingPasteboard().propertyList(forType: "NSFilenamesPboardType") as? NSArray,
+            let path = board[0] as? String
+            else { return false }
         
+        let suffix = URL(fileURLWithPath: path).pathExtension
+        for ext in self.expectedExt {
+            if ext.lowercased() == suffix {
+                return true
+            }
+        }
+        return false
     }
+    
     override func draggingExited(_ sender: NSDraggingInfo?) {
-        
+        self.layer?.backgroundColor = NSColor.gray.cgColor
     }
+    
+    override func draggingEnded(_ sender: NSDraggingInfo?) {
+        self.layer?.backgroundColor = NSColor.gray.cgColor
+    }
+    
     override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
-        Swift.print("kk")
-        return true
-    }
-    override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        guard let pasteboard = sender.draggingPasteboard().propertyList(forType: "NSFilenamesPboardType") as? NSArray,
+            let path = pasteboard[0] as? String
+            else { return false }
+        
+        //GET YOUR FILE PATH !!
+        self.filePath = path
+        Swift.print("FilePath: \(filePath)")
         
         return true
-    }
-    override func concludeDragOperation(_ sender: NSDraggingInfo?) {
-        
     }
 }
--- NSPastboard/ViewController.swift
+++ NSPastboard/ViewController.swift
@@ -16,7 +16,18 @@ class ViewController: NSViewController {
         
         
     }
-
+    override func viewDidAppear() {
+//        let openPanel = NSOpenPanel(contentRect: NSMakeRect(0, 0, 300, 300), styleMask: .closable, backing: .buffered, defer: true)
+//        openPanel.allowsMultipleSelection = false
+//        openPanel.canChooseFiles = true
+//        openPanel.canChooseDirectories = true
+//        openPanel.canCreateDirectories = true
+//        openPanel.directoryURL = NSURL .fileURL(withPath: "/Users/sycf_ios_13579/Downloads/VLCKit")
+//        self.view.window?.addChildWindow(openPanel, ordered: .)
+        let fileURL = NSURL.fileURL(withPath: "/Users/sycf_ios_13579/Downloads/VLCKit")
+//        let folderURL = fileURL.deletingLastPathComponent()
+        NSWorkspace.shared().open(fileURL)
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
